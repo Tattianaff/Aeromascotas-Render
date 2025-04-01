6e4a83ab2ba93a1711ae5126cd80fb2a138160d9
Tattianaff <tatisforero0404@gmail.com>
2025-03-31 20:12:18 -0500

    Asegura ejecución correcta del script en Render

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Instalar dependencias necesarias (por si acaso)
 pip install -r requirements.txt
+
+# Ejecutar migraciones
 python manage.py migrate
+
+# (Render se encarga de ejecutar gunicorn luego)
